0b1d883cbf436ae384a02bc2e0de306d781974cb
Sergey <ser.ser.05021997@gmail.com>
2019-04-01 09:51:04 +0300

    Hero control

---
+++ Assets/Scripts/heroController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class heroController : MonoBehaviour{
+
+    private Vector3 positionMove;
+    public float speed = 5.0f;
+
+    public float fireRate = 0.1f;
+    private float time = 0.0f;
+
+    public GameObject bullet;
+
+    private void Start(){
+        positionMove = transform.position;
+        time = fireRate;
+    }
+
+    private void Update(){
+        
+        UpdateFire();
+        UpdateInput();
+        UpdateMove();
+    }
+
+    private void UpdateInput(){
+        positionMove = UnityEngine.Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        positionMove.z = 0;
+    }
+
+    private void UpdateMove(){
+        float moveDelta = (positionMove - transform.position).magnitude;
+        if (moveDelta <= speed * Time.deltaTime)
+        {
+            transform.position = positionMove;
+            return;
+        }
+        Vector3 moveDir = positionMove - transform.position;
+        moveDir.Normalize();
+        transform.position += moveDir * speed * Time.deltaTime;
+    }
+
+    private void UpdateFire(){
+        fireRate -= Time.deltaTime;
+        if (fireRate <= 0.0f){
+            timerEnded();
+        }
+    }
+
+    void timerEnded(){
+       fireRate = time;
+       Instantiate (bullet, transform.position, transform.rotation);
+    }
+}
---
+++ Assets/Scripts/laser.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class laser : MonoBehaviour
+{
+    public float speedLaser = 3.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(0, speedLaser * Time.deltaTime, 0);
+    }
+}
